b59680c9844d2018083a49fa951700e3d702de9d
Minh Lam <kelvincoder@gmail.com>
2018-08-19 12:14:46 +0700

    Add repositories

---
+++ Shop.Data/Repositories/FooterRepository.cs
@@ -0,0 +1,17 @@
+﻿using Shop.Data.Infrastructure;
+using Shop.Model.Models;
+
+namespace Shop.Data.Repositories
+{
+    public interface IFooterRepository : IRepository<Footer>
+    {
+
+    }
+
+    public class FooterRepository : RepositoryBase<Footer>, IFooterRepository
+    {
+        public FooterRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ Shop.Data/Repositories/PostRepository.cs
@@ -0,0 +1,13 @@
+﻿using Shop.Data.Infrastructure;
+using Shop.Model.Models;
+
+namespace Shop.Data.Repositories
+{
+    public interface IPostRepository: IRepository<Post>
+    {
+    }
+
+    class PostRepository
+    {
+    }
+}
