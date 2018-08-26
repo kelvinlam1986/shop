c7bd1b7bebc07d41ae9b2278127c90b802d888d3
Minh Lam <kelvincoder@gmail.com>
2018-08-26 11:51:37 +0700

    Implement Post, Put, Delete, GetAll API for PostCategoryController

--- Shop.Web/Api/PostCategoryController.cs
+++ Shop.Web/Api/PostCategoryController.cs
@@ -3,9 +3,11 @@ using Shop.Service;
 using Shop.Web.Infrastructure.Core;
 using System.Net;
 using System.Net.Http;
+using System.Web.Http;
 
 namespace Shop.Web.Api
 {
+    [RoutePrefix("/api/postcategory")]
     public class PostCategoryController : ApiControllerBase
     {
         private IPostCategoryService _postCategoryService;
@@ -16,7 +18,25 @@ namespace Shop.Web.Api
             this._postCategoryService = postCategoryService;
         }
 
-        public HttpResponseMessage Create(HttpRequestMessage request, PostCategory postCategory)
+        public HttpResponseMessage Get(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var listCategory = _postCategoryService.GetAll();
+                    response = request.CreateResponse(HttpStatusCode.OK, listCategory);
+                }
+                return response;
+            });
+        }
+
+        public HttpResponseMessage Post(HttpRequestMessage request, PostCategory postCategory)
         {
             return CreateHttpResponse(request, () =>
             {
@@ -29,6 +49,44 @@ namespace Shop.Web.Api
                     var category = _postCategoryService.Add(postCategory);
                     _postCategoryService.SaveChanges();
                     response = request.CreateResponse(HttpStatusCode.Created, category);
+                }
+                return response;
+            });
+        }
+
+        public HttpResponseMessage Put(HttpRequestMessage request, PostCategory postCategory)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    _postCategoryService.Update(postCategory);
+                    _postCategoryService.SaveChanges();
+                    response = request.CreateResponse(HttpStatusCode.OK);
+                }
+                return response;
+            });
+        }
+
+        public HttpResponseMessage Delete(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    _postCategoryService.Delete(id);
+                    _postCategoryService.SaveChanges();
+                    response = request.CreateResponse(HttpStatusCode.OK);
                 }
                 return response;
             });
