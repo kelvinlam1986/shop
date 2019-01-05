ae27b115149d094e520f1eac027199444f67d3bb
Minh Lam <kelvincoder@gmail.com>
2019-01-05 21:11:20 +0700

    Refactor code

--- Shop.Web/app/app.js
+++ Shop.Web/app/app.js
@@ -1,22 +1,13 @@
-﻿/// <reference path="D:\Shop\Git\Shop.Web\Assets/admin/libs/angular/angular.js" />
-//(function () {
-//    angular.module('shop', ['shop.products', 'shop.common']).config(config);
-
-//    config.$inject = ['$stateProvider', '$urlRouterProvider'];
-//    function config($stateProvider, $urlRouterProvider) {
-//        $stateProvider.state('home', {
-//            url: '/admin',
-//            templateUrl: '/app/components/home/homeView.html',
-//            controller: 'homeController'
-//        });
-//        $urlRouterProvider.otherwise('/admin');
-
-//    }
-//})();
-
-(function () {
-    angular.module('shop', ['ui.router', 'shop.products', 'shop.common']).config(config);
+﻿(function () {
+    angular.module('shop', ['shop.products', 'shop.common']).config(config);
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
     function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: '/admin',
+            templateUrl: '/app/components/home/homeView.html',
+            controller: 'homeController'
+        });
+
+        $urlRouterProvider.otherwise('/admin');
     }
 })();
--- Shop.Web/app/components/products/products.module.js
+++ Shop.Web/app/components/products/products.module.js
@@ -1,20 +1,16 @@
 ﻿/// <reference path="D:\Shop\Git\Shop.Web\Assets/admin/libs/angular/angular.js" />
 
-//(function () {
-//    angular.module('shop.products', ['shop.common']).config(config);
-//    function config($stateProvider, $urlRouterProvider) {
-//        $stateProvider.state('products', {
-//            url: 'products',
-//            templateUrl: '/app/components/products/productListView.html',
-//            controller: 'productListController'
-//        }).state('product_add', {
-//            url: '/product_add',
-//            templateUrl: '/app/components/products/productAddView.html',
-//            controller: 'productAddController'
-//        })
-//    }
-//})();
-
 (function () {
-    angular.module('shop.products', []);
+    angular.module('shop.products', ['shop.common']).config(config);
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('products', {
+            url: '/products',
+            templateUrl: '/app/components/products/productListView.html',
+            controller: 'productListController'
+        }).state('product_add', {
+            url: '/producct_add',
+            templateUrl: '/app/components/products/productAddView.html',
+            controller: 'productAddController'
+        })
+    }
 })();
