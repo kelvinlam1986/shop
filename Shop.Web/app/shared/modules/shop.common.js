eaf45dd1f09916905a5ed51110115fd5590aef69
Minh Lam <kelvincoder@gmail.com>
2019-01-05 18:30:12 +0700

    Fix ui-router and inject module

--- Shop.Web/app/shared/modules/shop.common.js
+++ Shop.Web/app/shared/modules/shop.common.js
@@ -1,5 +1,9 @@
 ﻿/// <reference path="D:\Shop\Git\Shop.Web\Assets/admin/libs/angular/angular.js" />
 
+//(function () {
+//    angular.module('shop.common', ['ui-router'])
+//})();
+
 (function () {
-    angular.module('shop.common', ['ui-router'])
+    angular.module('shop.common', []);
 })();
