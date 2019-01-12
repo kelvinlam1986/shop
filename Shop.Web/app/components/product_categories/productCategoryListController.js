df024440751457ca4db4fa8b8263d4afb611223e
Minh Lam <kelvincoder@gmail.com>
2019-01-12 17:34:53 +0700

    Add notification to page

--- Shop.Web/app/components/product_categories/productCategoryListController.js
+++ Shop.Web/app/components/product_categories/productCategoryListController.js
@@ -2,8 +2,8 @@
 
 (function (app) {
     app.controller('productCategoryListController', productCategoryListController);
-    productCategoryListController.$inject = ['$scope', 'apiService']
-    function productCategoryListController($scope, apiService) {
+    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService']
+    function productCategoryListController($scope, apiService, notificationService) {
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
@@ -21,10 +21,16 @@
                 params: {
                     keyword: $scope.keyword,
                     page: page,
-                    pageSize: 2
+                    pageSize: 20
                 }
             }
             apiService.get('/api/productcategory/getall', config, function (result) {
+                if (result.data.TotalCount == 0) {
+                    notificationService.displaySuccess('Không có bảng ghi nào được tìm thấy');
+                } else {
+                    notificationService.displaySuccess('Tìm thấy ' + result.data.TotalCount + ' bảng ghi');
+                }
+
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
