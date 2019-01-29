d0bb30050b8caf074a9d50c80fe5c5291acd627d
Minh Lam <kelvincoder@gmail.com>
2019-01-29 09:54:41 +0700

    Set output cach for improve performance

--- Shop.Web/Controllers/HomeController.cs
+++ Shop.Web/Controllers/HomeController.cs
@@ -30,6 +30,7 @@ namespace Shop.Web.Controllers
             Mapper = mapper;
         }
 
+        [OutputCache(Duration = 60, Location = System.Web.UI.OutputCacheLocation.Server)]
         public ActionResult Index()
         {
             var slides = this._commonService.GetSlides();
@@ -50,21 +51,8 @@ namespace Shop.Web.Controllers
             return View(homeViewModel);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         [ChildActionOnly]
+        [OutputCache(Duration = 3600)]
         public ActionResult Footer()
         {
             var footer = this._commonService.GetFooter();
@@ -78,6 +66,8 @@ namespace Shop.Web.Controllers
             return PartialView();
         }
 
+        [ChildActionOnly]
+        [OutputCache(Duration = 3600)]
         public ActionResult Category()
         {
             var categories = _productCategoryService.GetAll();
